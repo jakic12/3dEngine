dc27f510944b9b8876187bdd719dd6569b489393
jakic12 <drusany.jakob@gmail.com>
2019-10-09 17:58:07 +0200

    added Camera, all convertions to the camera local coordinate system

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const gid = x => document.getElementById(x)
 
+if(!math){
+  console.log("Make sure that math.js is included above the script!");
+}
+
 /**
  * Prepare the canvas
  */
@@ -19,7 +23,6 @@ while(!window){
   canvas.height = document.body.clientHeight
 })();
 
-
 /**
  * An Array of numbers
  * @typedef {Array<Number>} Vector
@@ -33,6 +36,11 @@ while(!window){
 /**
  * A 3d point - 3d vector
  * @typedef {Array<Number>} Point
+ */
+
+/**
+ * A 2d local point on a image
+ * @typedef {Array<Number>} 2dPoint
  */
 
  /**
@@ -92,3 +100,102 @@ const toLocal = (X0, m, R, X, Hp) =>
 const R1 = getRotationMatrix(2,1,0 /* TODO: figure out why non zero values here, result in errors in the transformation */);
 const X0 = [1,1,0];
 console.log(toGlobal(X0, 1, R1, toLocal(X0, 1, R1, [2,3,4], [0,0])));
+
+function Camera(cameraOut, x = 0, y = 0, z = 0, w = 0, fi = 0, k = 0){
+  this.w = w;
+  this.fi = fi;
+  this.k = k;
+
+  this.x = x;
+  this.y = y;
+  this.z = z;
+
+  this.Hp = [0,0];
+  this.m = 1;
+
+  this.getRot = () => [this.w, this.fi, this.k]
+  this.getOrigin = () => [this.x,this.y,this.z]
+  this.getCameraRotationMatrix = () => getRotationMatrix(this.w, this.fi, this.k)
+
+  /**
+   * convert a local point in the global system
+   * @param {2dPoint} xp a point on the image
+   * @param {Number} m global scaling factor
+   */
+  this.localToGlobal = (xp) => 
+    toGlobal(this.getOrigin(), this.m, this.getCameraRotationMatrix(), xp)
+
+  /**
+   * !!not tested, probably doesn't work
+   * 
+   * @param {Point} O Origin of the custom system
+   * @param {Angle} w Angle of the custom system
+   * @param {Angle} fi Angle of the custom system
+   * @param {Angle} k Angle of the custom system
+   */
+  this.convertToCustomGlobal = (O, w, fi, k, xp) =>{
+    return toGlobal(
+      math.subtract(this.getOrigin(),O),
+      this.m,
+      getRotationMatrix(this.w - w, this.fi - fi, this.k - k),
+      xp
+    )
+  }
+
+  this.convertToCameraLocal = (X) => 
+    toLocal(this.getOrigin(), this.m, this.getCameraRotationMatrix(), X, this.Hp)
+
+  this.drawPoint = X => {
+    const xp = this.convertToCameraLocal(X)
+    const ctx = cameraOut;
+    ctx.strokeColor = "black"
+    ctx.beginPath();
+    ctx.arc(xp.get([0])-10, xp.get([1])-10, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+}
+
+const canvas = gid("canvas");
+const ctx = canvas.getContext('2d');
+const camera1 = new Camera(ctx);
+
+
+const cube = [
+  [0,0,0],
+  [0,0,300],
+  [0,300,0],
+  [0,300,300],
+  [300,0,0],
+  [300,0,300],
+  [300,300,0],
+  [300,300,300]
+]
+
+let i = 0;
+setInterval(() => {
+  for(const point of cube){
+    camera1.drawPoint(math.add(point, [500,300,300]))
+  }
+  i++;
+  if(i >= Math.PI*200){
+    i = 0
+  }
+})
+
+window.addEventListener("keydown", e => {
+  switch(e.key){
+    case "ArrowUp":
+      camera1.w -= 0.1
+      break;
+    case "ArrowDown":
+      camera1.w += 0.1
+      break;
+    case "ArrowLeft":
+      camera1.fi += 0.1
+      break;
+    case "ArrowRight":
+      camera1.fi -= 0.1
+      break;
+  }
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
